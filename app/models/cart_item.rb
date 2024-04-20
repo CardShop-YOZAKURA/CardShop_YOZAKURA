c99e4056292e7704fa2d8e5930a2a9f90a070f1e
EC2 Default User <141564706+Satoshi-1217@users.noreply.github.com>
2024-04-20 14:34:46 +0000

    model-edit

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,12 @@
 class CartItem < ApplicationRecord
+  
+  belongs_to :item
+  belongs_to :customer
+  
+  validates :item_id, :customer_id, :amount, presence: true
+  
+  # def subtotal
+  #   item.price_including_tax * amount
+  # end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,26 @@
 class Item < ApplicationRecord
+  
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
+  belongs_to :genre
+  
+  has_one_attached :image
+  
+  validates :name, :introduction, :price, presence: true
+  
+  validates :is_active, inclusion: { in: [true, false] }
+  
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  # def price_including_tax
+    # (self.price * 1.1).round
+  # end
+  
+  
 end
